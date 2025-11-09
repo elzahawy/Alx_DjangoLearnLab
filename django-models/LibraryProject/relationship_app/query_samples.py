71e46d8265b0654c657df19b49c439793eebff9c
elzahawy <keyuuelzahawii@gmail.com>
2025-11-09 18:52:16 +0300

    Move query_samples.py into relationship_app and fix queries

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,45 +1,17 @@
-import os
-import django
+# LibraryProject/relationship_app/query_samples.py
+from .models import Author, Book, Library, Librarian
 
-# Setup Django environment
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings")
-django.setup()
-
-from relationship_app.models import Author, Book, Library, Librarian
-
-# 1. Query all books by a specific author
 def books_by_author(author_name):
-    return Book.objects.filter(author__name=author_name)
+    # Get the author object
+    author = Author.objects.get(name=author_name)
+    # Return all books by this author
+    return Book.objects.filter(author=author)
 
-# 2. List all books in a library
 def books_in_library(library_name):
     library = Library.objects.get(name=library_name)
     return library.books.all()
 
-# 3. Retrieve the librarian for a library
 def librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
-import os
-import django
-
-# Setup Django environment
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings")
-django.setup()
-
-from relationship_app.models import Author, Book, Library, Librarian
-
-# 1. Query all books by a specific author
-def books_by_author(author_name):
-    return Book.objects.filter(author__name=author_name)
-
-# 2. List all books in a library
-def books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.books.all()
-
-# 3. Retrieve the librarian for a library
-def librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.librarian
+    return Librarian.objects.get(library=library)
 
