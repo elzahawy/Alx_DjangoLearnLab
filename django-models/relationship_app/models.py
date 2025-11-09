34ab8d62bb16a9b3edf8b2fa81dfb8490732de93
elzahawy <keyuuelzahawii@gmail.com>
2025-11-09 17:51:23 +0300

    Add Book model, admin registration, and seed sample data

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -6,12 +6,16 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
+from django.db import models
+
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    author = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
 
     def __str__(self):
         return self.title
+
 
 class Library(models.Model):
     name = models.CharField(max_length=255)
