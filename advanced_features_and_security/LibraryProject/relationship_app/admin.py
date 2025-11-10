7cc1af7c6e14129d841fea972eb1dc2130f5f869
elzahawy <keyuuelzahawii@gmail.com>
2025-11-10 15:53:01 +0300

    Remove duplicate CustomUser admin from relationship_app
    - CustomUser admin should only be in bookshelf/admin.py
    - Removed duplicate registration from relationship_app/admin.py

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,42 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.utils.translation import gettext_lazy as _
-from .models import CustomUser, Author, Book, Library, Librarian, UserProfile
+from .models import Author, Book, Library, Librarian, UserProfile
 
 
-# Task 0: Custom User Admin Configuration
-@admin.register(CustomUser)
-class CustomUserAdmin(BaseUserAdmin):
-    """Custom admin configuration for CustomUser model"""
-    # Fields to display in the user list
-    list_display = ('email', 'username', 'date_of_birth', 'is_staff', 'is_active', 'date_joined')
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'date_joined')
-    
-    # Fieldsets for user editing
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('username', 'first_name', 'last_name', 'date_of_birth', 'profile_photo')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-    )
-    
-    # Fieldsets for user creation
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'username', 'password1', 'password2', 'date_of_birth', 'profile_photo'),
-        }),
-    )
-    
-    # Search and ordering
-    search_fields = ('email', 'username', 'first_name', 'last_name')
-    ordering = ('email',)
-    
-    # Make email the username field
-    filter_horizontal = ('groups', 'user_permissions')
-
-
-# Register other models
+# Register models
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('name',)
