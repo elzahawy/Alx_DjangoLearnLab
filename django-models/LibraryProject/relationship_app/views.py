31cf72aeef9bb7caae0348865ceccce7371d4c90
elzahawy <keyuuelzahawii@gmail.com>
2025-11-10 13:59:23 +0300

    Fix DetailView import to match ALX checker requirements
    - Changed from django.views.generic import DetailView
    - To: from django.views.generic.detail import DetailView
    - This matches the exact import pattern the automated checker expects

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
