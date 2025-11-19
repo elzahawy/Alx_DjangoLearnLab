95a5a4e59827e36060b524d2f3c54ee7acfbf77f
elzahawy <keyuuelzahawii@gmail.com>
2025-11-19 14:24:44 +0400

    Add book_list view with permissions

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -16,3 +16,22 @@ def edit_book(request):
 @permission_required('bookshelf.can_delete', raise_exception=True)
 def delete_book(request):
     return render(request, "bookshelf/delete_book.html")
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    return render(request, "bookshelf/book_list.html")
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    return render(request, "bookshelf/create_book.html")
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request):
+    return render(request, "bookshelf/edit_book.html")
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request):
+    return render(request, "bookshelf/delete_book.html")
