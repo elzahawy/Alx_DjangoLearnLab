e22b54c698ac38c03a6f449f2a62dcbb966c3c46
elzahawy <keyuuelzahawii@gmail.com>
2025-11-19 14:19:38 +0400

    Add custom permissions, views with decorators, and README for ALX Task 1

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -54,3 +54,20 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view books"),
+            ("can_create", "Can create books"),
+            ("can_edit", "Can edit books"),
+            ("can_delete", "Can delete books"),
+        ]
+
+    def __str__(self):
+        return self.title
