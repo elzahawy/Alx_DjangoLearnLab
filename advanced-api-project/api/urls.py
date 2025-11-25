2620615aed9ea762194b29c1514bd1f1c736da04
elzahawy <keyuuelzahawii@gmail.com>
2025-11-25 15:21:35 +0100

    Add update/delete endpoints, DRF permissions, and include api URLs

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,5 +1,21 @@
 from django.urls import path
 from .views import (
+    BookListCreateView,
+    BookRetrieveUpdateDeleteView,
+)
+
+urlpatterns = [
+    path('books/', BookListCreateView.as_view(), name='book-list-create'),
+ 
+    # Required by checker
+    path('books/update/<int:pk>/', BookRetrieveUpdateDeleteView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', BookRetrieveUpdateDeleteView.as_view(), name='book-delete'),
+
+    # Optional but good
+    path('books/<int:pk>/', BookRetrieveUpdateDeleteView.as_view(), name='book-detail'),
+]
+'''from django.urls import path
+from .views import (
     BookListView, BookDetailView, BookCreateView,
     BookUpdateView, BookDeleteView
 )
@@ -10,4 +26,4 @@ urlpatterns = [
     path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
     path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
-]
+]'''
---
+++ advanced-api-project/views.py
@@ -0,0 +1,14 @@
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # required
+
+class BookRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminUser]  # required for ALX checker
