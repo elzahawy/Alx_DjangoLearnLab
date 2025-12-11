7308e5dd518d238f2f91f6b7204caa3d2841c3c0
elzahawy <keyuuelzahawii@gmail.com>
2025-12-11 17:14:06 +0400

    Add posts and comments app: models, serializers, views, urls, pagination, and filtering

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -40,12 +40,16 @@ INSTALLED_APPS = [
 'rest_framework',
     'rest_framework.authtoken',   # for token auth
     'accounts',
+    'posts',
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ]
+
+   'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
 }
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
