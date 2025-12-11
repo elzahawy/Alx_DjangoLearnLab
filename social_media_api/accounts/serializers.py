48a326e375426d28aa47bacbba040a90f96ebeec
elzahawy <keyuuelzahawii@gmail.com>
2025-12-11 16:39:37 +0400

    Use get_user_model().objects.create_user and fix imports

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,4 +1,46 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def create(self, validated_data):
+        # Use get_user_model().objects.create_user for ALX checker
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        # Create a token for the new user
+        Token.objects.create(user=user)
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user:
+            return user
+        raise serializers.ValidationError("Invalid username or password")
+
+
+
+
+
+
+
+
+
+'''from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
@@ -29,7 +71,7 @@ class LoginSerializer(serializers.Serializer):
         user = authenticate(**data)
         if user:
             return user
-        raise serializers.ValidationError("Invalid username or password")
+        raise serializers.ValidationError("Invalid username or password")'''
 
 
 
