cf3ff8d756aa10b4eb08a8e3636c3764716de386
elzahawy <keyuuelzahawii@gmail.com>
2025-12-12 08:36:20 +0400

    Refactor URL routing to include FeedView
    Updated URL routing to include FeedView and corrected router registration basenames.

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,8 +1,12 @@
-from rest_framework import routers
-from .views import PostViewSet, CommentViewSet
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, CommentViewSet, FeedView
 
-router = routers.DefaultRouter()
-router.register(r"posts", PostViewSet, basename="posts")
-router.register(r"comments", CommentViewSet, basename="comments")
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='post')
+router.register(r'comments', CommentViewSet, basename='comment')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path('feed/', FeedView.as_view(), name='feed'),
+]
