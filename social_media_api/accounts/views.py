1db7769bc6657db707ebbfbc9fd7696131a000bd
elzahawy <keyuuelzahawii@gmail.com>
2025-12-12 08:56:36 +0400

    Import generics and permissions from rest_framework

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -2,7 +2,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework import generics, permissions
 from .serializers import RegisterSerializer, LoginSerializer
 from django.shortcuts import get_object_or_404
 from .models import CustomUser
