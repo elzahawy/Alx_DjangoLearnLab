295cbf6e5b662d7d7e578a95bd3e27a7c26b0318
elzahawy <keyuuelzahawii@gmail.com>
2025-12-12 08:20:33 +0400

    Refactor permissions and pagination in views

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,46 +1,44 @@
 from rest_framework import viewsets, permissions, filters
+from rest_framework.pagination import PageNumberPagination
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-from rest_framework.permissions import IsAuthenticated
 
 
-class FeedView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        # Get posts from users the current user follows
-        followed_users = request.user.following.all()
-        posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 50
 
 
-
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    """Custom permission to only allow owners of an object to edit/delete it."""
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to allow only authors to edit or delete their posts/comments
+    """
     def has_object_permission(self, request, view, obj):
+        # Read-only requests are allowed for any user
         if request.method in permissions.SAFE_METHODS:
             return True
+        # Only author can edit/delete
         return obj.author == request.user
 
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
+    queryset = Post.objects.all().order_by("-created_at")
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+    pagination_class = StandardResultsSetPagination
     filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'content']
+    search_fields = ["title", "content"]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by('-created_at')
+    queryset = Comment.objects.all().order_by("-created_at")
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+    pagination_class = StandardResultsSetPagination
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-# Create your views here.
