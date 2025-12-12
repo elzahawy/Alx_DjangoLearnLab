125da2fd28cfba5742009e8fab819f4ea843bbac
elzahawy <keyuuelzahawii@gmail.com>
2025-12-12 08:19:36 +0400

    Refactor URL routing for posts and comments

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,12 +1,8 @@
-from rest_framework.routers import DefaultRouter
-from django.urls import path, include
-from .views import PostViewSet, CommentViewSet, FeedView
+from rest_framework import routers
+from .views import PostViewSet, CommentViewSet
 
-router = DefaultRouter()
-router.register(r'posts', PostViewSet, basename='post')
-router.register(r'comments', CommentViewSet, basename='comment')
+router = routers.DefaultRouter()
+router.register(r"posts", PostViewSet, basename="posts")
+router.register(r"comments", CommentViewSet, basename="comments")
 
-urlpatterns = [
-    path('', include(router.urls)),
-    path('feed/', FeedView.as_view(), name='feed'),
-]
+urlpatterns = router.urls
