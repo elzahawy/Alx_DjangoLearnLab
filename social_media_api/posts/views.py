0a0488d12a456451ec8bb8703cf25f4f3c78aea8
elzahawy <keyuuelzahawii@gmail.com>
2025-12-12 08:34:25 +0400

    Add FeedView to retrieve posts from followed users

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -42,3 +42,19 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+
+
+class FeedView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        followed_users = request.user.following.all()
+        posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+
